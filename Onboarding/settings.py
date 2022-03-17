eb53092518e62a02b6aa880d686e681494f7a3a6
Sripad Kulkarni <sripad.kulkarni@salesforce.com>
2022-03-17 18:40:17 +0530

    New resource add for managers and trail guides

--- Onboarding/settings.py
+++ Onboarding/settings.py
@@ -119,11 +119,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-'''
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 MEDIA_URL= os.environ['FASTLY_CDN_URL'] + '/'
-'''
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
