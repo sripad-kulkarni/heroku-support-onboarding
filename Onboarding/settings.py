550afdd7e35ed28bf8655b2ed0cd2edd0911540a
Sripad Kulkarni <sripad.kulkarni@salesforce.com>
2022-03-17 10:53:07 +0530

    CDN added

--- Onboarding/settings.py
+++ Onboarding/settings.py
@@ -122,3 +122,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+MEDIA_URL= os.environ['FASTLY_CDN_URL']
