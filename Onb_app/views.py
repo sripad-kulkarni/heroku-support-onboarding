816c3463e3f449390b0aeb732cf34647d3cf89d6
Sripad Kulkarni <sripad.kulkarni@salesforce.com>
2022-04-03 21:21:52 +0530

    Password Reset Done, Password Change Done, Email setup using Gmail done

--- Onb_app/views.py
+++ Onb_app/views.py
@@ -755,7 +755,7 @@ def reset_password(request):
 		if form.is_valid():
 			data = form.cleaned_data['email']
 			try:
-				result = User.objects.get(email = data)
+				result = Profile.objects.get(email = data)
 			except:
 				messages.warning(request, "We're unable to fetch details for the email requested here. Please recheck and try again!")
 				return render(request, 'reset_password.html', {'form': form})
